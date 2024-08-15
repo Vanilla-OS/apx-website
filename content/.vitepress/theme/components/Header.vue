2f409fc7233f8667d1c9aa78a23eaa038c38a2fa
Gabriel Cozma <me@gabs.eu.org>
2024-08-15 16:54:56 +0300

    cleanup: organise content and migrate each to their own file

---
+++ content/.vitepress/theme/components/Header.vue
@@ -0,0 +1,66 @@
+<template>
+    <nav class="flex flex-col gap-0 bg-[#101010] w-full">
+        <div
+            class="flex flex-col sm:flex-row items-center justify-between px-4 sm:px-16 py-5 border-b border-white border-opacity-20"
+        >
+            <a href="/">
+                <img
+                    src="/assets/logo.svg"
+                    alt="Apx"
+                    class="h-8 mb-4 sm:mb-0"
+                />
+            </a>
+            <div
+                class="flex flex-row items-center gap-4 sm:gap-8 font-light text-base opacity-80"
+            >
+                <a
+                    v-for="link in theme.nav.links.first"
+                    :key="link"
+                    :href="link.url"
+                    class="text-white"
+                    >{{ link.text }}</a
+                >
+            </div>
+        </div>
+        <div
+            class="flex flex-col sm:flex-row items-center justify-between px-4 sm:px-16 py-2"
+        >
+            <p class="text-white opacity-40 font-normal mb-4 sm:mb-0">
+                {{ theme.nav.version }}
+            </p>
+            <div
+                class="flex flex-col sm:flex-row items-center gap-4 sm:gap-8 font-light opacity-80 text-base"
+            >
+                <div class="flex flex-row items-center gap-4">
+                    <a
+                        v-for="link in theme.nav.links.second"
+                        :key="link"
+                        :href="link.url"
+                        class="text-white"
+                        >{{ link.text }}</a
+                    >
+                </div>
+                <div
+                    class="flex flex-row sm:flex-row gap-2 justify-center items-center mt-4 sm:mt-0"
+                >
+                    <a
+                        href="https://github.com/Vanilla-OS/apx"
+                        class="border border-white text-white px-4 py-2 rounded-full text-base hover:bg-white hover:text-black transition duration-200"
+                        >Source Code</a
+                    >
+                    <a
+                        href="https://github.com/Vanilla-OS/apx/releases/latest"
+                        class="bg-green-400 text-black px-4 py-2 rounded-full text-base hover:bg-green-500 transition duration-200"
+                        >Download</a
+                    >
+                </div>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script setup lang="ts">
+import { useData } from 'vitepress';
+
+const { theme } = useData();
+</script>
---
+++ content/.vitepress/theme/views/Home.vue
@@ -0,0 +1,145 @@
+<template>
+    <div class="flex flex-col gap-32 mb-10 pb-44">
+        <section
+            id="what-is"
+            class="flex flex-col gap-8 text-center justify-center items-center"
+        >
+            <h2 class="text-3xl lg:text-5xl font-semibold">
+                What is <span class="text-green-500">Apx</span>?
+            </h2>
+            <p class="text-xl lg:text-2xl font-normal lg:w-[35rem]">
+                Apx is a platform that streamlines dev environments into
+                portable, reproducible subsystems, seamlessly integrating them
+                with the system.
+            </p>
+            <picture>
+                <source
+                    media="(prefers-color-scheme: dark)"
+                    srcset="/assets/components/what-is-dark.svg"
+                />
+                <source
+                    media="(prefers-color-scheme: light)"
+                    srcset="/assets/components/what-is.svg"
+                />
+                <img
+                    alt="What is Apx?"
+                    src="/assets/components/what-is.svg"
+                    class="lg:w-[50rem]"
+                />
+            </picture>
+        </section>
+        <section
+            id="subsystems"
+            class="flex flex-col lg:flex-row flex-wrap gap-12 justify-center items-center"
+        >
+            <picture>
+                <source
+                    media="(prefers-color-scheme: dark)"
+                    srcset="/assets/components/subsystems-dark.svg"
+                />
+                <source
+                    media="(prefers-color-scheme: light)"
+                    srcset="/assets/components/subsystems.svg"
+                />
+                <img
+                    src="/assets/components/subsystems.svg"
+                    alt="Subsystems"
+                    class="w-[10rem] lg:w-[25rem]"
+                />
+            </picture>
+            <div class="flex flex-col gap-4 text-center lg:text-left">
+                <h2 class="text-3xl lg:text-5xl font-semibold">
+                    What are
+                    <span class="text-green-500 dark:text-green-300"
+                        >Subsystems</span
+                    >?
+                </h2>
+                <p class="text-xl lg:text-2xl font-normal lg:w-[40rem]">
+                    Apx subsystems are portable, reproducible dev environments
+                    that can be shared and reused across projects.
+                </p>
+            </div>
+        </section>
+        <section
+            id="vscode"
+            class="flex flex-col flex-wrap gap-12 justify-center items-center text-center"
+        >
+            <picture>
+                <source
+                    media="(prefers-color-scheme: dark)"
+                    srcset="/assets/components/vscode-dark.svg"
+                />
+                <source
+                    media="(prefers-color-scheme: light)"
+                    srcset="/assets/components/vscode.svg"
+                />
+                <img src="/assets/components/vscode.svg" alt="VSCode" />
+            </picture>
+            <p class="text-xl lg:text-2xl lg:w-[35rem]">
+                Apx integrates them with your system, providing that native
+                feedback you're used to.
+            </p>
+        </section>
+        <section
+            id="stack"
+            class="flex flex-row flex-wrap gap-12 justify-center items-center"
+        >
+            <picture>
+                <source
+                    media="(prefers-color-scheme: dark)"
+                    srcset="/assets/components/stack-dark.png"
+                />
+                <source
+                    media="(prefers-color-scheme: light)"
+                    srcset="/assets/components/stack.png"
+                />
+                <img
+                    src="/assets/components/stack.png"
+                    alt="Stack"
+                    class="w-[10rem] lg:w-[25rem]"
+                />
+            </picture>
+            <div class="flex flex-col gap-4 text-center lg:text-left">
+                <h2 class="text-2xl lg:text-5xl font-semibold">
+                    Share the perfect
+                    <span class="text-green-500">Stack</span>
+                </h2>
+                <div
+                    class="flex flex-col gap-12 text-xl lg:text-2xl font-normal lg:w-[40rem]"
+                >
+                    <p>
+                        Subsystems area created using stacks, a set of
+                        instructions that defines exactly what must be included
+                        in your subsystem.
+                    </p>
+                    <p>
+                        Got the best configuration for your project? Share it
+                        with your colleagues so that they can replicate it!
+                    </p>
+                </div>
+            </div>
+        </section>
+        <section
+            id="what-is"
+            class="flex flex-col gap-40 text-center justify-center items-center"
+        >
+            <div
+                class="flex flex-col gap-8 text-center justify-center items-center"
+            >
+                <h2 class="text-2xl lg:text-6xl font-semibold">
+                    What is your
+                    <span class="text-green-500">distribution</span>?
+                </h2>
+                <p class="text-xl lg:text-2xl font-normal lg:w-[40rem]">
+                    Does not matter. Base your subsystems on top of any Linux
+                    distribution, all you need is a Docker image, that’s all.
+                </p>
+            </div>
+            <img
+                src="/assets/components/distributions.svg"
+                alt="Multiple distributions"
+                class="w-[100rem]"
+            />
+        </section>
+    </div>
+</template>
