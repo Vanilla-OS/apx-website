97e2fc3e10d7448f92d2ffd4f89db7197c2145c9
Gabriel Cozma <me@gabs.eu.org>
2024-10-16 17:38:55 +0300

    fix: header in light mode

--- content/.vitepress/theme/components/Header.vue
+++ content/.vitepress/theme/components/Header.vue
@@ -7,7 +7,7 @@
 >
   <nav>
     <div
-      class="flex flex-row items-center justify-between border-b border-white border-opacity-20 px-4 py-5 sm:px-16"
+      class="flex flex-row items-center justify-between border-b border-white border-opacity-20 px-4 py-5 sm:px-16 text-black bg-[#101010]"
     >
       <a href="/">
         <img src="/assets/logo.svg" alt="Apx" class="h-8" />
@@ -32,7 +32,7 @@
       </div>
     </div>
     <div
-      class="hidden flex-col items-center justify-between px-4 py-2 sm:flex-row sm:px-16 lg:flex"
+      class="hidden flex-col items-center justify-between px-4 py-2 sm:flex-row sm:px-16 lg:flex bg-[#101010]"
     >
       <p class="mb-4 font-normal text-white opacity-40 sm:mb-0">
         {{ theme.nav.version }}
